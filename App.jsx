a49031ae23e3cacb7e75865fc622bac4d632aecd
patricio2306 <patricio.munoz27@gmail.com>
2025-06-25 13:04:45 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <img src="/logo.png" alt="Equilibra" width="150" />
+      <h1>Bienvenido a Equilibra</h1>
+      <p>Tu plataforma de teleconsulta psicológica y profesional.</p>
+    </div>
+  );
+}
